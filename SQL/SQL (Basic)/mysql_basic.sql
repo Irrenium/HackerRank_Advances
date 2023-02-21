a3fb546c56420db739700ef1351a20834998fe0d
Reyad <riadox41@gmail.com>
2023-02-21 01:39:10 +0100

    Updated my_sql file

--- SQL/SQL (Basic)/mysql_basic.sql
+++ SQL/SQL (Basic)/mysql_basic.sql
@@ -16,7 +16,31 @@ SELECT * FROM CITY;
 
 --Japanese Cities' Names
 
---
+--Weather Observation Station 1
+
+--Weather Observation Station 3
+
+--Weather Observation Station 4
+
+--Weather Observation Station 6
+
+--Weather Observation Station 7
+
+--Weather Observation Station 8
+
+--Weather Observation Station 9
+
+--Weather Observation Station 10
+
+--Weather Observation Station 11
+
+--Weather Observation Station 12
+
+--Higher Than 75 Marks
+
+--Employee Names
+
+--Employee Salaries
 
 
 
