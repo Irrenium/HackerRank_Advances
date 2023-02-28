6be78d9955088a696d404044f6ebf9ed55c3a56c
Reyad <riadox41@gmail.com>
2023-02-28 23:48:35 +0100

    Created Day21 file

---
+++ 30 days of code/Day21.java
@@ -0,0 +1,9 @@
+
+class Printer<E> {
+    // Write your code here
+    public <E> void printArray(E[] array) {
+        for (E element : array) {
+            System.out.println(element);
+        }
+    }
+}
