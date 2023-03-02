2bd4043b2d8851475e9f2c5976ec10c1f729243a
Reyad <riadox41@gmail.com>
2023-03-02 23:31:58 +0100

    Created Day28 file

--- 30 days of code/Day28.c
+++ 30 days of code/Day28.c
@@ -11,14 +11,13 @@
 #include <ctype.h>
 
 
-
 int mycmp(const char *a, const char *b) {
     const char *cp1 = a, *cp2 = b;
 
     for (; toupper(*cp1) == toupper(*cp2); cp1++, cp2++)
         if (*cp1 == '\0')
             return 0;
-            
+
     return ((toupper(*cp1) < toupper(*cp2)) ? -1 : +1);
 } 
 
